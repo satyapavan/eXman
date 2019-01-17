f98b67ed7cd79f43239f02070725a0c12e678496
Satya Pavan <pavan.simham@gmail.com>
2019-01-17 23:27:56 +0530

    implemented bhim api

--- metadata.js
+++ metadata.js
@@ -4,8 +4,8 @@ const SS_HH_EE_EE_TT_II_DD = '1I9xBLU7LZ3ZRO6pe0DdtUGgvgNrBF3sRPxbOVQiSFpE';
 
 // 1DueNTkz7YAAWurv--t0hTmeEOtFxJOUUqPUEHCSZz24
 
-const ARRAY_TX_TYPE = [ 'Cash', 'Credit Card', 'ICICI', 'PayTm', 'Sodexo'];
-const ARRAY_TX_CATEGORY = ['Daily Needs', 'Transport', 'Home Maintainance', 'Health', 'Eating Out', 'Entertainment', 'Clothing', 'Office', 'Others'];
+const ARRAY_TX_TYPE = [ 'Cash', 'Credit Card', 'ICICI', 'PayTm', 'Sodexo', 'BHIM'];
+const ARRAY_TX_CATEGORY = ['Daily Needs', 'Transport', 'Home Maintainance', 'Health', 'Eating Out', 'Entertainment', 'Clothing', 'Office', 'Binami', 'Others'];
 const MONTH_NUM_TO_MMM = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const TX_CATEGORY_ICONS = [
@@ -17,6 +17,7 @@ const TX_CATEGORY_ICONS = [
     '<i class="icon-tx-category mx-1 my-1 icon-entertainment fas fa-theater-masks"></i>'  , // Entertainment
     '<i class="icon-tx-category mx-1 my-1 icon-clothing fas fa-tshirt"></i>'     , // Clothing
     '<i class="icon-tx-category mx-1 my-1 icon-office fas fa-briefcase"></i>'  , // Office
+    '<i class="icon-tx-category mx-1 my-1 icon-binami fas fa-exchange-alt"></i>', // Binami
     '<i class="icon-tx-category mx-1 my-1 icon-others fas fa-usd-circle"></i>'   // Others
 ];
 
@@ -25,7 +26,8 @@ const TX_TYPE_ICONS = [
     '<img src="icon-citibank.png" class="icon-tx-type icon-citi">',
     '<img src="icon-icici.png" class="icon-tx-type icon-icici">',
     '<img src="icon-paytm.png" class="icon-tx-type icon-paytm">',
-    '<img src="icon-sodexo.png" class="icon-tx-type icon-sodexo">'
+    '<img src="icon-sodexo.png" class="icon-tx-type icon-sodexo">',
+    '<img src="icon-bhim.png" class="icon-tx-type icon-bhim">'
 ];
 
 // Array of API discovery doc URLs for APIs used by the quickstart
